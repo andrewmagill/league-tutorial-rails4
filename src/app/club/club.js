2ff8440167eee2e576e4e25795d492cfad8c1e11
Andrew Magill <andrew.b.magill@gmail.com>
2014-06-02 00:09:38 -0500

    add real backend and mock backend.

--- src/app/club/club.js
+++ src/app/club/club.js
@@ -1,7 +1,7 @@
 /**
  * Club module
  */
-angular.module('league.club', ['ui.state'])
+angular.module('league.club', ['ui.state', 'ngResource'])
 
 /**
  * Define the route that this module relates to, and the page template and controller that is tied to that route
@@ -22,10 +22,15 @@ angular.module('league.club', ['ui.state'])
 /**
  * And of course we define a controller for our route.
  */
-.controller('ClubsCtrl', function ClubsController($scope) {
-  $scope.clubs = [
-    {name: "hard coded club 1", contact_officer: "hard coded officer 1"},
-    {name: "hard coded club 2", contact_officer: "hard coded officer 2"}
-  ];
-});
+.controller('ClubsCtrl', function ClubsController($scope, ClubRes) {
+  $scope.clubs = ClubRes.query();
+})
+
+/**
+ * Add a resource to allow us to get at the server
+ */
+.factory('ClubRes', function ($resource) {
+  return $resource('../clubs.json');
+})
+;
  
--- src/app/club/club.spec.js
+++ src/app/club/club.spec.js
@@ -2,6 +2,8 @@
  * Unit test for the club functionality
  */
 describe('Clubs list controller', function() {
+  var scope, httpBackend;
+  
   //mock Applciation to allow us to inject our own dependencies
   beforeEach(angular.mock.module('league'));
   
@@ -10,12 +12,25 @@ describe('Clubs list controller', function() {
     //create an empty scope
     scope = $rootScope.$new();
     
+    scope.httpBackend = _$httpBackend_;
+    scope.httpBackend.expect('GET', 
+                             '../clubs.json').respond([
+                             {"contact_officer":"Officer 1","created_at":"2012-02-02T00:00:00Z","date_created":"2012-01-01T00:00:00Z","id":1,"name":"Club 1","updated_at":"2012-03-03T00:00:00Z"},
+                             {"contact_officer":"Officer 2","created_at":"2012-02-02T00:00:00Z","date_created":"2012-01-01T00:00:00Z","id":2,"name":"Club 2","updated_at":"2012-03-03T00:00:00Z"}]);
+
     //declare the controller and inject our empty scope
     $controller('ClubsCtrl', {$scope: scope});
   }));
   
   // test start here
   it('Has two clubs defined', function() {
+    scope.$digest();
+    scope.httpBackend.flsuh();
     expect(scope.clubs.length).toEqual(2);
   });
+  
+  it('First club\'s contact office is as expected', funciton () {
+    scope.$digest();
+    scope.httpBackend.flush();
+    expect(scope.clubs[0].content_officer).toEqual('Officer 1);
 });
